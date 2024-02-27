8b19ea77159fce34d6f4e46a8f2646cff8e77cff
Moses <mosesmkhonza@outlook.com>
2024-02-27 13:00:08 -0800

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,8 +10,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+	{
 
 	func(tree->n); /* Call the function for the current node */
 	binary_tree_preorder(tree->left, func); /* Recur on the left subtree */
 	binary_tree_preorder(tree->right, func); /* Recur on the right subtree */
+	}
 }
