fd22e48b82c71e97a7a43b64c797cf669e8a3072
Moses <mosesmkhonza@outlook.com>
2024-02-29 14:24:55 -0800

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,13 +6,13 @@
  * Description: The value in the node must be passed as a parameter to func.
  * If tree or func is NULL, do nothing
  */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL && func == NULL)
+	if (tree && func)
 	{
-
-		func(tree->n); /* Call the function for the current node */
-		binary_tree_preorder(tree->left, func); /* Recur on the left subtree */
-		binary_tree_preorder(tree->right, func); /* Recur on the right subtree */
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
