0a99fc756786db0e829ff43db157cf32c5303eff
Moses <mosesmkhonza@outlook.com>
2024-02-29 13:34:17 -0800

    16

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,12 +2,11 @@
 
 /**
  * binary_tree_uncle - Finds the uncle of a node
- *                     in a binary tree.
- * @node: A pointer to the node to find the uncle of.
+ * @node: Pointer to the node to find the uncle
  *
- * Return: If node is NULL or has no uncle, NULL.
- *         Otherwise, a pointer to the uncle node.
+ * Return: Pointer to the uncle node, or NULL if node is NULL or has no uncle
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL ||
